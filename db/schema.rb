14fd7491d2ab6d6de7c294751e0c1682d50bcf12
Kevin Leland <lelandk@gmail.com>
2014-05-08 10:19:31 -0400

    scaffolded team

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140508135804) do
+ActiveRecord::Schema.define(version: 20140508141721) do
 
   create_table "students", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.integer  "age"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "teams", force: true do |t|
+    t.integer  "number_teams"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
